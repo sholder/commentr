11a3322ec8f80be29c812b840e7f43e9ef243e1a
Stephen Holder <sholder@google.com>
2008-08-28 11:19:42 -0700

    ui cleanup tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def logged_in?
+    return !session[:user_id].nil?
+  end
+  
+  def admin?
+    return false unless logged_in?
+    @user ||= User.find(session[:user_id])
+    @user.is_admin
+  end
+
 end
