30d8afb8fea9d3c73ebd23831bf14676fb152c44
Stephen Holder <sholder@google.com>
2008-08-28 14:29:55 -0700

    capifying, getting ready for deploy

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :application, "commentr"
+
+set :scm, :git
+set :repository,  "git://github.com/sholder/commentr.git"
+set :branch, 'master'
+set :deploy_via, :remote_cache
+
+set :deploy_to, "/home/sholder/webapps/commentr"
+
+set :user, 'sholder'
+set :ssh_options, { :forward_agent => true }
+
+role :app, "teamholder.com"
+role :web, "teamholder.com"
+role :db,  "teamholder.com", :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
